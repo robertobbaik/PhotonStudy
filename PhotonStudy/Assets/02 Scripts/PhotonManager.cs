6abc5872e49a3a35c53699af825a1e30e7ac9d24
robertobbaik <robertobbaik@gmail.com>
2024-05-08 23:19:29 +0900

    NetworkRunner

--- PhotonStudy/Assets/02 Scripts/PhotonManager.cs
+++ PhotonStudy/Assets/02 Scripts/PhotonManager.cs
@@ -7,11 +7,13 @@ using Fusion.Sockets;
 using System;
 using TMPro;
 
-public class PhotonManager : MonoBehaviour, INetworkRunnerCallbacks
+public class PhotonManager : MonoBehaviour, IPlayerJoined, IPlayerLeft
 {
     public Button button_JoinRoom;
     public TextMeshProUGUI text_Status;
+    public TextMeshProUGUI text_RoomName;
     public GameObject playerPrefab;
+    public NetworkRunner networkRunner;
 
     void Start()
     {
@@ -31,97 +33,18 @@ public class PhotonManager : MonoBehaviour, INetworkRunnerCallbacks
 
     }
 
-    public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
+    public async void GameStart(GameMode mode, string roomName, string sceneName)
     {
-        throw new NotImplementedException();
+        
     }
 
-    public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
+    public void PlayerJoined(PlayerRef player)
     {
-        throw new NotImplementedException();
+        string message = "Join Player : " + player.PlayerId;
+        text_Status.text = message;
     }
 
-    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
-    {
-        Debug.Log("Join Player : " + player.PlayerId);
-    }
-
-    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
-    {
-        Debug.Log("Leave Player : " + player.PlayerId);
-    }
-
-    public void OnInput(NetworkRunner runner, NetworkInput input)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnConnectedToServer(NetworkRunner runner)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnSceneLoadDone(NetworkRunner runner)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnSceneLoadStart(NetworkRunner runner)
+    public void PlayerLeft(PlayerRef player)
     {
         throw new NotImplementedException();
     }
