8013dfc805e06d716cd5e5f9640fc599caefcf1d
robertobbaik <robertobbaik@gmail.com>
2024-05-09 11:54:08 +0900

    Photon Set

--- PhotonStudy/Assets/02 Scripts/PhotonManager.cs
+++ PhotonStudy/Assets/02 Scripts/PhotonManager.cs
@@ -7,7 +7,7 @@ using Fusion.Sockets;
 using System;
 using TMPro;
 
-public class PhotonManager : MonoBehaviour, IPlayerJoined, IPlayerLeft
+public class PhotonManager : MonoBehaviour, IPlayerJoined, IPlayerLeft, INetworkRunnerCallbacks
 {
     public Button button_JoinRoom;
     public TextMeshProUGUI text_Status;
@@ -23,8 +23,18 @@ public class PhotonManager : MonoBehaviour, IPlayerJoined, IPlayerLeft
         });
     }
 
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
     public void OnClick_JoinRoom()
     {
+        Debug.Log("OnClick");
+        text_RoomName.text = "On Click";
         GameStart(GameMode.Shared, "");
     }
 
@@ -40,16 +50,116 @@ public class PhotonManager : MonoBehaviour, IPlayerJoined, IPlayerLeft
         };
 
         await networkRunner.StartGame(startGameArgs);
+        text_RoomName.text = roomName;
 
+        Debug.Log(networkRunner.IsConnectedToServer);
     }
 
     public void PlayerJoined(PlayerRef player)
     {
+        Debug.LogWarning("Join Player");
+       
+
         string message = "Join Player : " + player.PlayerId;
         text_Status.text = message;
     }
 
     public void PlayerLeft(PlayerRef player)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+    {
+        Debug.LogWarning("I NetworkRunner Callback : Join ");
+    }
+
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnInput(NetworkRunner runner, NetworkInput input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnConnectedToServer(NetworkRunner runner)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnSceneLoadDone(NetworkRunner runner)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnSceneLoadStart(NetworkRunner runner)
     {
         throw new NotImplementedException();
     }
