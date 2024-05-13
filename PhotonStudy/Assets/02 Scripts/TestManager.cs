38655e79770d9090f13bd39113833d3e1d1e0a73
robertobbaik <robertobbaik@gmail.com>
2024-05-13 19:23:52 +0900

    Test Message

--- PhotonStudy/Assets/02 Scripts/TestManager.cs
+++ PhotonStudy/Assets/02 Scripts/TestManager.cs
@@ -2,9 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
+using UnityEngine.UI;
 
 public class TestManager : NetworkBehaviour
 {
+    [Rpc(sources:RpcSources.InputAuthority, targets:RpcTargets.StateAuthority)]
+    public void TestMessage(string nickName)
+    {
+        Debug.Log("nickname : " + nickName);
+    }
+
     void Update()
     {
         
